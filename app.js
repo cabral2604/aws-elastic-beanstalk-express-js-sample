383169ec3eea6ecaa465cc1ebbae7e899216fc90
cabral2604 <82717931+cabral2604@users.noreply.github.com>
2023-12-06 19:15:06 -0500

    changes on app clinique

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('I want to make 110K + this year!'));
+app.get('/', (req, res) => res.send('La clinique Claudange sera pas ouverte entre 10-15h'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
